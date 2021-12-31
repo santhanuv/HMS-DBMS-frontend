b90a4b5c8f78f5a65da82b34c5049fc24de665a8
santhanuv17 <santhanuv17@gmail.com>
2021-12-31 18:52:55 +0530

    Notification Completed

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { MdNotifications } from "react-icons/md";
 
 function Notification() {
-  return <div></div>;
+  return (
+    <div className="relative">
+      <button>
+        <span
+          className="bg-primaryGrey text-white absolute right-[-2px] top-[-5px] rounded-[100px] w-[30px] 
+      aspect-square text-center text-xl"
+        >
+          3
+        </span>
+        <MdNotifications className="text-5xl" />
+      </button>
+    </div>
+  );
 }
 
 export default Notification;
